2c11e4842aa4f7d2c4757012dab7797a2faec282
Andrew <topor.9090@gmail.com>
2020-02-12 08:58:10 +0200

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Andrey Sokolov on 12.02.2020.
+//  Copyright © 2020 Andrey Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
